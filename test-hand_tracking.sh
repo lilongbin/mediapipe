9155d92ef3934d61cc94c9ffee769d6439a7afcc
Li Longbin <longbin.li@aptiv.com>
2023-06-25 20:16:57 -0700

    add env setup steps

--- test-hand_tracking.sh
+++ test-hand_tracking.sh
@@ -1,9 +1,31 @@
 #! /bin/bash
 #
 ####################################################
-# Author      : user
+# Author      : longbin
 # Created date: 2023-06-20 21:58:48
 ####################################################
+
+# 版本要求
+# Python >= 3.9.0
+# Bazel >= 3.7.2, (< 4.0.0 for iOS)
+# GCC/G++ >= 8.0.0 (Linux, macOS)
+
+# 1. OpenCV3安装
+# cd mediapipe
+# chmod +x setup_opencv.sh
+# ./setup_opencv.sh
+
+# 如果安装libdc1394-22-dev失败，则把脚本中libdc1394-22-dev改为libdc1394-dev
+
+# 2. 安装MediaPipe所需依赖库
+# pip --default-timeout=1000 install -r requirements.txt
+
+# 3. Bazel下载与安装
+# Bazel下载地址 https://github.com/bazelbuild/bazel/tags
+# 以下载 4.2.1 版本为例 https://github.com/bazelbuild/bazel/releases/tag/4.2.1
+
+# Bazel 安装：
+# bash bazel-4.2.1-installer-linux-x86_64.sh --user
 
 build_dir=build
 graph_path=mediapipe/graphs/hand_tracking/hand_tracking_desktop_live.pbtxt
